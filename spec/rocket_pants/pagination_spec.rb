44a60cdad7493e065c6b37a7dac922e9c4a496d7
Alessandro Desantis <desa.alessandro@gmail.com>
2016-07-07 18:42:58 +0200

    Add missing tests

--- spec/rocket_pants/pagination_spec.rb
+++ spec/rocket_pants/pagination_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe 'RocketPants::Pagination' do
   end
 
   describe '#expose_with_pagination' do
-    let(:collection) do
+    let(:paginated_collection) do
       Class.new do
         def count; 1; end
         def total_pages; 2; end
@@ -42,7 +42,7 @@ RSpec.describe 'RocketPants::Pagination' do
 
     before(:each) do
       allow(ActiveModel::Serializer::ArraySerializer).to receive(:new)
-        .with(collection)
+        .with(paginated_collection)
         .and_return(serialized_collection)
     end
 
@@ -50,19 +50,67 @@ RSpec.describe 'RocketPants::Pagination' do
       expect(subject).to receive(:expose)
         .with(a_hash_including(
           test_collection: serialized_collection,
-          count: collection.count,
+          count: paginated_collection.count,
           pagination: {
-            pages: collection.total_pages,
-            current: collection.current_page,
-            count: collection.count,
-            per_page: collection.per_page,
-            previous: collection.previous_page,
-            next: collection.next_page
+            pages: paginated_collection.total_pages,
+            current: paginated_collection.current_page,
+            count: paginated_collection.count,
+            per_page: paginated_collection.per_page,
+            previous: paginated_collection.previous_page,
+            next: paginated_collection.next_page
           }
         ))
         .once
 
-      subject.expose_with_pagination(test_collection: collection)
+      subject.expose_with_pagination(test_collection: paginated_collection)
+    end
+  end
+
+  describe '#paginate_and_expose' do
+    let(:collection) do
+      Class.new do
+      end.new
+    end
+
+    let(:paginated_collection) do
+      Class.new do
+        def count; 1; end
+        def total_pages; 2; end
+        def current_page; 3; end
+        def per_page; 4; end
+        def previous_page; 5; end
+        def next_page; 6; end
+      end.new
+    end
+
+    let(:serialized_collection) { instance_double('ActiveModel::Serializer::ArraySerializer') }
+
+    before(:each) do
+      allow(collection).to receive(:paginate)
+        .and_return(paginated_collection)
+
+      allow(ActiveModel::Serializer::ArraySerializer).to receive(:new)
+        .with(paginated_collection)
+        .and_return(serialized_collection)
+    end
+
+    it 'paginates and exposes the collection with pagination metadata' do
+      expect(subject).to receive(:expose)
+        .with(a_hash_including(
+          test_collection: serialized_collection,
+          count: paginated_collection.count,
+          pagination: {
+            pages: paginated_collection.total_pages,
+            current: paginated_collection.current_page,
+            count: paginated_collection.count,
+            per_page: paginated_collection.per_page,
+            previous: paginated_collection.previous_page,
+            next: paginated_collection.next_page
+          }
+        ))
+        .once
+
+      subject.paginate_and_expose test_collection: collection
     end
   end
 end
